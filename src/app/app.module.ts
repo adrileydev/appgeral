8ee8b0ab40ddb93401ebbd7a80abc869908f9693
adriley <adriley@outlook.com>
2019-02-15 00:01:36 -0200

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 0d652d7fda629b18e8c2631f2eca4fa190dc652e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,37 +6,25 @@ import { AppComponent }   from './app.component';
 import {AlertModule}      from 'ngx-bootstrap';
 import { FormsModule }    from '@angular/forms';
 
-<<<<<<< Updated upstream
 import { HomeComponent } from './home/home.component';
 import { ClienteComponent } from './cliente/cliente.component';
 import { DatabaseService } from './Database/Database.service';
-=======
->>>>>>> Stashed changes
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< Updated upstream
     HomeComponent,
     ClienteComponent
-=======
-
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
     AlertModule.forRoot(),
     FormsModule
 
-
   ],
-<<<<<<< Updated upstream
   providers: [DatabaseService],
-=======
-  providers: [],
->>>>>>> Stashed changes
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,8 +4,8 @@ import { DatabaseService } from '../Database/Database.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
-
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   cursos: string[] = [];
